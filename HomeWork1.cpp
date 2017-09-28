c4b5375248fa8753d0a3819195dc0123b07aea9b
Aleksandr Vorona <32168978+CrowAlcoholic@users.noreply.github.com>
2017-09-29 00:46:05 +0300

    Update HomeWork1.cpp

--- HomeWork1.cpp
+++ HomeWork1.cpp
@@ -7,7 +7,7 @@ int main()
     std::cout << "Task #8" << std::endl << "Enter a three-digit number" << std::endl;
     int givenNumber1 = 0;
     std::cin >> givenNumber1;
-    int n1 =givenNumber1 % 10;
+    int n1 = givenNumber1 % 10;
     int n10 = ((givenNumber1 - n1) % 100);
     int n100 = (givenNumber1 - n1 - n10);
     std::cout << n100 + n10 / 10 + n1 * 10 << std::endl;
