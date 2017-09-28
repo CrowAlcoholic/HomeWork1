564d99a3da60a32c965e907f66b391004a684ebc
Aleksandr Vorona <32168978+CrowAlcoholic@users.noreply.github.com>
2017-09-28 19:42:42 +0300

    Update HomeWork1.cpp

--- HomeWork1.cpp
+++ HomeWork1.cpp
@@ -5,15 +5,15 @@ int main()
 {
     std::cout << "Var.#5" << std::endl;
     std::cout << "Task #8" << std::endl << "Enter a three-digit number" << std::endl;
-    int k = 0;
-    std::cin >> k;
-    int n1 = k % 10;
-    int n10 = ((k - n1) % 100);
-    int n100 = (k - n1 - n10);
+    int givenNumber1 = 0;
+    std::cin >> givenNumber1;
+    int n1 =givenNumber1 % 10;
+    int n10 = ((givenNumber1 - n1) % 100);
+    int n100 = (givenNumber1 - n1 - n10);
     std::cout << n100 + n10 / 10 + n1 * 10 << std::endl;
     /////////////////////////////////////////////////////////////////////////////////////
     std::cout << "Task #17" << std::endl;
-    int d = 564;
-    std::cout << "Answer: X = "  << (d - d % 10) / 10 + (d % 10) * 100 << std::endl;
+    int result = 564;
+    std::cout << "Answer: X= "  << (result - result % 10) / 10 + (result % 10) * 100 << std::endl;
     return 0;
 }
